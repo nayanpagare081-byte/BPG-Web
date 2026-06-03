a39f955236c04e626747c61c038ffaec03993c6c
Developer <dev@bpg.com>
2026-06-04 03:19:06 +0530

    Cleanup unwanted files

--- getUsers.js
+++
@@ -1,8 +0,0 @@
-const Database = require('better-sqlite3');
-const path = require('path');
-
-const dbPath = path.join(process.cwd(), 'dev.db');
-const db = new Database(dbPath);
-
-const users = db.prepare('SELECT id, email, role FROM User').all();
-console.log(JSON.stringify(users, null, 2));
